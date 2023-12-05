a90fd0b53289e97f5c6fe18f55f5e95f4c9a479a
Axl-Gomez-2909 <148070790+Axl-Gomez-2909@users.noreply.github.com>
2023-12-04 20:39:33 -0600

    Plantillas

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Menú</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container mt-5">
+        <h2>Menú</h2>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="formularios_action/formulario_productos.php">Formulario de Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="formularios_action/formulario_categorias.php">Formulario de Categorías</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="formularios_action/formulario_ordenes.php">Formulario de Órdenes</a>
+            </li>
+        </ul>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
